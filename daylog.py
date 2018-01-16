093b2600a319ed05cc525e7ef42e1602d0405142
Hideyo Kawahara <tmpsys.sangitan@gmail.com>
2018-01-16 09:41:56 +0900

    Add logging and bugfix upload

--- daylog.py
+++ daylog.py
@@ -9,6 +9,7 @@
 from google.appengine.api import memcache   # Memcache API
 from datetime import datetime as dt         # datatime型
 import json                                 # jsonファイル操作
+import logging                              # ログ出力
 
 import gcs                                  # GCS操作
 
@@ -19,20 +20,26 @@ class daylog:
         @json jsonデータ
     """
 
-    def __init__(self, id, date):
+    def __init__(self, id, date, mode):
         """ 指定された日付のデータを読み込む
             @id   参照するセンサのID
             @date 参照する日付
         """
         self.id = id
         self.date = dt.strptime(date[0:8], '%Y%m%d')
-        if self.read() != 1:
+        if mode == 'W' and self.read() != 1:
             self.new()
 
     def gen_key(self):
         """ キーの生成
         """
         return "date_" + self.id + "_" + self.date.strftime('%Y%m%d')
+
+    def log(self, cmd, ret):
+        if ret == 0:
+            logging.info('DAYLOG ' + cmd + ' ' + self.gen_key() + ' SUCCESS')
+        else:
+            logging.warning('DAYLOG ' + cmd + ' ' + self.gen_key() + ' ERROR')
 
     def write(self, date, fi, bv, val, ad):
         """ 日誌に書き込む
@@ -44,7 +51,7 @@ class daylog:
         """
 
         # jsonを辞書型に変換
-        dic = json.loads(logjson)
+        dic = json.loads(self.read())
 
         # dateを文字列型→日時型→文字列型に再変換
         date = dt.strptime(date, '%Y%m%d%H%M%S')
@@ -79,13 +86,19 @@ class daylog:
         # キャッシュ問い合わせ
         logjson = memcache.get(self.gen_key())
         if logjson is None:
+            self.log('READ MEMCACHE', 1)
+
             # ないならGCSから読み込み
             try:
                 logjson = gcs.read_file(self.gen_key() + ".json")
             except:
+                self.log('READ STORAGE', 1)
                 return None
             else:
+                self.log('READ STORAGE', 0)
                 memcache.add(self.gen_key(), logjson)
+        else:
+            self.log('READ MEMCACHE', 0)
 
         return logjson
 
