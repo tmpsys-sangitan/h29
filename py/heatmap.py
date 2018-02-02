7b27fcbf9ce6e58f20194b354becd3d66e7b761a
Yoshida Hikaru <tmpsys.sangitan@gmail.com>
2018-02-02 12:19:26 +0900

    Add save latest data

--- py/heatmap.py
+++ py/heatmap.py
@@ -7,8 +7,10 @@
 # NAME        :Hikaru Yoshida
 '''
 
+from google.appengine.api import memcache   # Memcache API
 import random           # 乱数
 
+from py import diary   # センサ管理
 from py import sensor   # センサ管理
 from py import utility  # 汎用関数
 
@@ -23,7 +25,11 @@ def get_latest(sensor_type):
 
     # マップIDごとの配列生成
     for mapid in sensor.get_list_mapid(sensor_type):
-        res_dic[mapid] = round(random.uniform(10.0, 35.0), 1)
+        res_dic[mapid] = memcache.get(diary.keycache(None, sensor.get_devid(mapid, sensor_type)))
+        # ↓↓↓ 実稼働時に削除
+        if res_dic[mapid] is None:
+            res_dic[mapid] = round(random.uniform(10.0, 35.0), 1)
+        # ↑↑↑ ここまで
 
     # JSONに変換して出力
     return utility.dump_json(res_dic)
